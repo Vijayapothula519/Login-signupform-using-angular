11245ba02f493868754ae4a4c3f57a3349a6c9ca
vijaya pothula <lakshmivijayapothula@gmail.com>
2024-04-06 12:07:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // Import necessary form-related modules
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crt';
+
+  signupUsers:any[] =[];
+  signupObj: any =  {
+    userName:"",
+    email:"",
+    password:"",
+  };
+
+  loginObj:any ={
+    userName:"",
+    password:"",
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void{
+    const localData = localStorage.getItem("signupUsers");
+    if(localData!=null){
+      this.signupUsers=JSON.parse(localData);
+    }
+  }
+
+  onSignup(){
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem("signUpUsers",JSON.stringify(this.signupUsers));
+    this.signupObj={
+      userName:"",
+      email:"",
+      password:"",
+    };
+  }
+
+  onLogin(){
+    const isUserExist=this.signupUsers.find(m=>m.userName==this.loginObj.userName && m.password==this.loginObj.password)
+    if(isUserExist!=undefined){
+      alert("User Login success");
+      //this.router.navigate(['/home']);
+      
+    }
+    else{
+      alert("wrong credentials entered")
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,FormsModule,
+    MatInputModule,MatFormFieldModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
